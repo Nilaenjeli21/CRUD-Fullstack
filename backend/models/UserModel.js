43b6be10786bc14c771939536ead96b8e6c5baea
Nila Enjeli <nilaanjeli1904@gmail.com>
2024-03-09 14:06:31 +0700

    fullstack

---
+++ backend/models/UserModel.js
@@ -0,0 +1,17 @@
+import {DataTypes, Sequelize} from "sequelize";
+import db from "../config/database.js";
+
+const {DataType} = Sequelize;
+
+const User = db.define('users',{
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    gender: DataTypes.STRING
+}, {
+    freezeTableName:true
+});
+export default User;
+
+(async()=>{
+    await db.sync();
+})();
